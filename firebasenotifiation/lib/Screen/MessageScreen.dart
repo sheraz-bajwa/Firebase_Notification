7f4ef007e5d3e1ebfb1db3a0f9300109b573a3e3
sheraz-bajwa <sheezybajwa@gmail.com>
2024-02-22 14:54:32 +0500

    first commit

---
+++ firebasenotifiation/lib/Screen/MessageScreen.dart
@@ -0,0 +1,18 @@
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:firebasenotifiation/Screen/Landing.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+class MessageScreen extends StatefulWidget {
+  const MessageScreen({super.key});
+
+  @override
+  State<MessageScreen> createState() => _MyWidgetState();
+}
+
+class _MyWidgetState extends State<MessageScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
--- firebasenotifiation/lib/main.dart
+++ firebasenotifiation/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:firebase_messaging/firebase_messaging.dart';
-import 'package:firebasenotifiation/Landing.dart';
+import 'package:firebasenotifiation/Screen/Landing.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
